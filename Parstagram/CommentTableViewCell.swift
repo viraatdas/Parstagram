6b433a4c9bd8173173fcb46b173aeaf812a6a726
Viraat Das <viraat.laldas@gmail.com>
2020-03-01 19:01:20 -0500

    done with project

---
+++ Parstagram/CommentTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommentTableViewCell.swift
+//  Parstagram
+//
+//  Created by Viraat Das on 3/1/20.
+//  Copyright © 2020 Viraat Das. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
